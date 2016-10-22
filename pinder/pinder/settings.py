1e2893d7e38de9327766bd88152641c6126255a4
Daniel Quinn <code@danielquinn.org>
2016-10-22 15:52:10 +0200

    Added a management command stub for Hannes

--- pinder/pinder/settings.py
+++ pinder/pinder/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
     "peering_requests.apps.PeeringRequestsConfig",
     "users.apps.UsersConfig",
+    "pinder",
 
     "rest_framework",
     "crispy_forms",
@@ -92,20 +93,21 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+# AUTH_PASSWORD_VALIDATORS = [
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+#     },
+#     {
+#         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+#     },
+# ]
+AUTH_PASSWORD_VALIDATORS = []  # meh
 
 
 # Internationalization
